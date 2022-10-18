57ac681aa395f006c10811df314a2136e83e6410
javimac-dev <javimac.dev@gmail.com>
2022-10-17 19:48:42 -0500

    no message

---
+++ PatronMVVM/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PatronMVVM.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PatronMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage =  new NavigationPage(new Page1());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PatronMVVM/ViewModels/VMPage1.cs
@@ -0,0 +1,98 @@
+﻿using PatronMVVM.Views;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace PatronMVVM.ViewModels
+{
+    public class VMPage1: BaseViewModel
+    {
+        #region VARIABLES
+        string _mensaje;
+        string _numero1;
+        string _numero2;
+        string _respuesta;
+        string _datopicker;
+        #endregion
+
+        #region CONSTRUCTOR /*no se toca*/
+        public VMPage1(INavigation navigation)
+        {
+            Navigation = navigation;/*se comporta como una página en navegación*/
+        }
+        #endregion 
+
+        #region CONTROLES
+        public string txtNumero1
+        {
+            get { return _numero1; } //retorna información de tipo string
+            set { SetValue(ref _numero1, value); }//recibe información de tipo string
+        }
+
+        public string txtNumero2
+        {
+            get { return _numero2; } //retorna información de tipo string
+            set { SetValue(ref _numero2, value); }//recibe información de tipo string
+        }
+
+        public string lblRespuesta
+        {
+            get { return _respuesta; } //retorna información de tipo string
+            set { SetValue(ref _respuesta, value); }//recibe información de tipo string
+        }
+
+        public string Datopicker
+        {
+            get { return _datopicker; } //retorna información de tipo string
+            set { SetValue(ref _datopicker, value); }//recibe información de tipo string
+        }
+        public string cboxPicker //este es el nombre en el binding
+        {
+            get { return _datopicker; } 
+            set 
+            { 
+                SetProperty(ref _datopicker, value);
+                Datopicker = _datopicker;
+            }
+        }
+
+
+
+
+        #endregion
+
+        #region FUNCIONES
+        public async Task fnPagina2()
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+
+/*        public async Task procesoAsincrono()
+        {
+ 
+        }
+*/
+
+        public void fnSumar()
+        {
+            double _num1=0;
+            double _num2=0;   
+            double _resp = 0;
+            _num1 = Convert.ToDouble(txtNumero1);
+            _num2 = Convert.ToDouble(txtNumero2);
+            _resp = _num1 + _num2;
+            lblRespuesta = _resp.ToString();
+        }
+        #endregion
+
+        #region COMANDOS
+        public ICommand btnPagina2 => new Command(async () => await fnPagina2());//ejecuta la función que está en la región de procesos 
+        public ICommand btnSumar => new Command(fnSumar);
+        #endregion
+    }
+}
